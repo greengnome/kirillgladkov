f31ada8b8a92382e10c1a31f30e12d426bfbc5aa
Kirill Gladkov <crosfire2011@gmail.com>
2021-07-31 21:46:27 +0300

    configure eslint

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,15 +1,14 @@
 import classes from 'styles/Navigation.module.scss';
 
-const Navigation = () => {
-  return (
-    <nav className={classes.navigation}>
-      <ul className={classes.menu}>
-        <li className={classes['menu--item']}>Home</li>
-        <li className={classes['menu--item']}>Experience</li>
-        <li className={classes['menu--item']}>Contact</li>
-      </ul>
-    </nav>
-  );
-};
+const Navigation = () => (
+  <nav className={classes.navigation}>
+    <ul className={classes.menu}>
+      <li className={classes['menu--item']}>Home</li>
+      <li className={classes['menu--item']}>Experience</li>
+      <li className={classes['menu--item']}>Contact</li>
+    </ul>
+    <div onClick={() => {}}>dd</div>
+  </nav>
+);
 
 export default Navigation;
