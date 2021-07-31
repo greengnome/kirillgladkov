8acd184e073903f8abee02e3590ca51239aa1476
Kirill Gladkov <crosfire2011@gmail.com>
2021-07-31 16:39:45 +0300

    created Main page styles

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from 'components/Header';
 import Teaser from 'components/Teaser';
 
+import classes from 'styles/Main.module.scss';
+
 export default function Home() {
   return (
-    <>
+    <main id={classes.container}>
       <Header />
       <Teaser />
-    </>
+    </main>
   );
 }
