a3cef3f45ddf21e35e27b22511fc0d9184473aba
Kirill Gladkov <crosfire2011@gmail.com>
2022-02-02 23:33:01 +0200

    fix layout component

--- components/Layout.js
+++ components/Layout.js
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 import { motion } from "framer-motion";
 
 const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
+  hidden: { opacity: 0, x: 0, y: 0 },
   enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: -100 },
+  exit: { opacity: 0, x: 0, y: 0 },
 };
 
 const Layout = ({ children }) => (
-  <div>
+  <div className="h-screen w-screen">
     <motion.main
       initial="hidden"
       animate="enter"
